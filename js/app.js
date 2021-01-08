501e683de0fd1cca828de7f2c52ad1f4377f5325
Simon Charters <simon259@hotmail.co.uk>
2021-01-08 14:36:25 +0000

    have title author and category diplaying

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+  const theForm = document.querySelector('#new-item-form');
+  theForm.addEventListener('submit', handleFormSubmit);
+
+  
 })
+
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+  const newListItem = document.createElement('li');
+  newListItem.textContent = `${event.target.title.value} ${event.target.author.value} ${event.target.category.value}`;
+
+  const list = document.querySelector('#reading-list');
+  list.appendChild(newListItem);
+
+
+}
